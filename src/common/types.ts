ec7bc23296f32c31716c7c67baf53f68bf3008f3
Margarita <Margarita>
2022-12-23 19:44:34 +0300

    feat: add types

---
+++ src/common/types.ts
@@ -0,0 +1,20 @@
+export interface PrimaryCheckboxProps{
+	id: string
+	title: string
+}
+
+export interface ProductCardProps{
+	title: string
+	img: string
+	price: number
+}
+
+export interface FullProductProps{
+	id: string
+	title: string
+	annotation: string
+	category: string
+	author: string
+	price: number
+	images: string[]
+}
