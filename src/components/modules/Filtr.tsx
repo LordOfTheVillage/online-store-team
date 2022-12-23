54b77c0d40af96900304c41d5dbb7ff063f26a16
Margarita <Margarita>
2022-12-23 23:34:13 +0300

    fix: add ability to add any content in filtr section

--- src/components/modules/Filtr.tsx
+++ src/components/modules/Filtr.tsx
@@ -1,24 +1,20 @@
 import React from "react"
-import { GENRES } from "../../../src/common/data"
-// import DualSlider from "../simple/DualSlider"
-// import { DualSlider } from "../simple/DualSlider"
 import { PrimaryButton } from "../simple/PrimaryButton"
-import { PrimaryCheckbox } from "../simple/PrimaryCheckbox"
 import { PrimaryInput } from "../simple/PrimaryInput"
-import { FiltrSection } from "./FiltrSection"
+import { FiltrSection } from "./FiltrSection";
 
 interface FiltrProps{
-	
+	sections:Array<{sectionsContent: JSX.Element[] | JSX.Element; title: string;}>
 }
 
-export const Filtr: React.FC<FiltrProps> = () => {
+export const Filtr: React.FC<FiltrProps> = (props) => {
+
+
 
   return (
 		<div className="filtr-module">
 			
-			<FiltrSection title="Category" >{GENRES.map((item) => <PrimaryCheckbox title={item.title} id={item.id}/>)}</FiltrSection>
-			{/* <FiltrSection title="Category" >{<DualSlider min={0} max={10} onChange={({ min, max }: {min: number, max: number}) => console.log(`min = ${min}, max = ${max}`)}/>}</FiltrSection> */}
-			
+			{props.sections.map(item => <FiltrSection title={item.title}>{item.sectionsContent}</FiltrSection>)}
 			<PrimaryInput title="Search"/>
 			<div className="col-2">
 				<PrimaryButton title="Clear"/>
--- src/components/pages/CatalogPage.tsx
+++ src/components/pages/CatalogPage.tsx
@@ -1,9 +1,10 @@
 import React from "react"
-import { ALL_PRODUCTS, SELECT_ITEMS } from "../../common/data"
+import { ALL_PRODUCTS, GENRES, SELECT_ITEMS } from "../../common/data"
 import { Catalog } from "../modules/Catalog"
 import { Filtr } from "../modules/Filtr"
 import { Header } from "../modules/Header"
 import {Sort} from "../modules/Sort"
+import { PrimaryCheckbox } from "../simple/PrimaryCheckbox"
 
 interface CatalogPageProps{
 	
@@ -11,11 +12,13 @@ interface CatalogPageProps{
 
 const CatalogPage: React.FC<CatalogPageProps> = () => {
 
+	const sectionsArr = [{sectionsContent: GENRES.map((item) => <PrimaryCheckbox title={item.title} id={item.id}/>), title: 'title1'}, {sectionsContent: <PrimaryCheckbox title={'00'} id={'99'}/>, title: 'title2'}]
+
   return (
 		<div className="catalog-page wrapper">
 			<Header/>
 			<div className="container page-content catalog-content">
-				<Filtr/>
+				<Filtr sections={sectionsArr}/>
 				<div className="catalog-content__second-item">
 					<Sort options={SELECT_ITEMS}/>
 					<Catalog productsCards = {ALL_PRODUCTS}/>
