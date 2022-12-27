32f344b6aab8425a083601837e3f0b4e66f7906a
Margarita <Margarita>
2022-12-27 16:48:59 +0300

    feat: update primary button interface

--- src/components/simple/PrimaryButton.tsx
+++ src/components/simple/PrimaryButton.tsx
@@ -2,9 +2,10 @@ import React from "react"
 
 interface PrimaryButtonProps{
 	title: string
+	onClick?: () => void
 }
 
-export const PrimaryButton: React.FC<PrimaryButtonProps> = (props) => {
+export const PrimaryButton: React.FC<PrimaryButtonProps> = ({title, onClick}) => {
 
-  return <button className="btn btn-primary">{props.title}</button>
+  return <button className="btn btn-primary" onClick = {onClick}>{title}</button>
 }
