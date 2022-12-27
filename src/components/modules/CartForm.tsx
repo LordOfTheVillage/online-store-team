885cd0dd726df28bc66fe94571fdc9f9a4663b2b
Margarita <Margarita>
2022-12-27 16:49:34 +0300

    feat: add click event on cart form button

--- src/components/modules/CartForm.tsx
+++ src/components/modules/CartForm.tsx
@@ -3,17 +3,22 @@ import { PrimaryButton } from "../simple/PrimaryButton"
 import { PrimaryInput } from "../simple/PrimaryInput"
 
 
+
 interface CartFormProps{
 	amount: number
 	totalPrice: number
+	changeModalClasses: () => void
 }
 
-export const CartForm: React.FC<CartFormProps> = ({amount, totalPrice}) => {
+export const CartForm: React.FC<CartFormProps> = ({amount, totalPrice, changeModalClasses}) => {
+
+	// const [modalClasses, setModalClasses] = useState({classModal: "modal", classContent: "modal-content"});
 
 	return(<div className="cart__form">
 		<div>Products: {amount}</div>
 		<div>Total Price: ${totalPrice}</div>
 		<PrimaryInput title="Enter promo code"/>
-		<PrimaryButton title="Buy"/>
+		<PrimaryButton title="Buy" onClick = {() => changeModalClasses()}/>
+		
 	</div>)
 }
