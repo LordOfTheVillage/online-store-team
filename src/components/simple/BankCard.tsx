3fc12a06123ced8c5a82d14196e1b0cf1749259b
Margarita <Margarita>
2023-01-05 14:55:42 +0300

    feat: add validation after click

--- src/components/simple/BankCard.tsx
+++ src/components/simple/BankCard.tsx
@@ -1,16 +1,60 @@
-import React from "react"
+import React, {  useState } from "react"
+import { inputData } from "src/common/types"
 import { PrimaryInput } from "./PrimaryInput"
 
-export const BankCard: React.FC = () => {
+interface bankCardProps {
+	onChange: (e: inputData[]) => void
+	cardData: inputData[]
+}
 
-  return <div className="card">
+export const BankCard: React.FC<bankCardProps> = ({onChange, cardData}) => {
+
+	const [cardValue, setCardValue] = useState(cardData);
+	const [logo, setLogo] = useState("card__logo");
+
+	const changeCardNumber = (e: React.FormEvent<HTMLInputElement>) => {
+		
+		const cardNumber = e.toString();
+		setCardValue(cardValue.map(item => {if(item.id === 'cardNumber') item.value = cardNumber; return item}))
+
+		if(cardNumber[0] === '4') setLogo(`card__logo card__logo-visa`);
+		else if(cardNumber[0] === '5') setLogo(`card__logo card__logo-mastercard`);
+		else if(cardNumber[0] === '6') setLogo(`card__logo card__logo-unionpay`);
+		else setLogo(`card__logo`);
+	}
+
+	const changeCardDate = (e: React.FormEvent<HTMLInputElement>) => {
+		let cardDate = e.toString();
+		setCardValue(cardValue.map(item => {if(item.id === 'date') item.value = cardDate; return item}))
+	}
+
+	const handleCardData = (e: React.FormEvent) => {
+		onChange(cardValue)
+	}
+
+  return <form className="card" onChange={handleCardData}>
 		<div className="card__row">
-			<div className="card__logo"></div>
-			<PrimaryInput title="Card number"/>
-		</div>
+			<div className={logo}></div>
+			<PrimaryInput key={cardValue[0].id} 
+									id={cardValue[0].id} 
+									title={cardValue[0].title} 
+									pattern={cardValue[0].pattern}
+									value={cardValue[0].value} 
+									onChange={(e: React.FormEvent<HTMLInputElement>) => changeCardNumber(e)}/></div>
 		<div className="card__row">
-			<PrimaryInput title="Valid Thru"/>
-			<PrimaryInput title="Code"/>
+		<PrimaryInput key={cardValue[1].id} 
+									id={cardValue[1].id} 
+									title={cardValue[1].title} 
+									pattern={cardValue[1].pattern}
+									isDate={true}
+									value={cardValue[1].value} 
+									onChange={(e: React.FormEvent<HTMLInputElement>) => changeCardDate(e)}/>
+		<PrimaryInput key={cardValue[2].id} 
+									id={cardValue[2].id} 
+									title={cardValue[2].title} 
+									pattern={cardValue[2].pattern}
+									value={cardValue[2].value} 
+									onChange={(e: React.FormEvent<HTMLInputElement>) => setCardValue(cardValue.map(item => {if(item.id === 'code') item.value = e.currentTarget.value; return item}))}/>
 		</div>
-	</div>
+	</form>
 }
