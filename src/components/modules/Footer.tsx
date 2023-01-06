f5611e683980b227d12fbb38f16525e9b189c766
Margarita <Margarita>
2023-01-06 22:20:18 +0300

    feat: update footer

--- src/components/modules/Footer.tsx
+++ src/components/modules/Footer.tsx
@@ -6,7 +6,16 @@ export const Footer: React.FC = () => {
 
   return (
     <footer className="footer">
-			Created with love <span>♡</span>
+			<div className="container col-3">
+				<div className="footer__item">
+					<a href="https://rs.school/js/" className="footer__rs"></a>
+				</div>
+				<div className="footer__item"><div>Created with love <span>♡</span></div><div style={{fontSize: '17px', marginTop: '3px'}}>2023</div></div>
+				<div className="footer__item">
+					<a href="https://github.com/LordOfTheVillage" className="footer__git"></a>
+					<a href="https://github.com/Margarita-Sipach" className="footer__git"></a>
+				</div>
+			</div>
 		</footer>
   )
 }
