c7260cd0570b03882f76b10a8f2d35c85fe99c90
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-01-12 13:35:32 +0300

    feat: update button tests and add input tests

--- src/tests/PrimaryButton.test.tsx
+++ src/tests/PrimaryButton.test.tsx
@@ -5,9 +5,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { PrimaryButton } from '../components/simple/PrimaryButton';
 
-test('should display a blank login form, with remember me checked by default', async () => {
-  const value = 'test';
-  const { findByText } = render(<PrimaryButton title={value} />);
-  const button = await findByText(value);
-  expect(button).toBeInTheDocument();
+describe('Primary button tests', () => {
+  test('should create button with text', async () => {
+    const value = 'test';
+    const { findByText } = render(<PrimaryButton title={value} />);
+    const button = await findByText(value);
+    expect(button).toBeInTheDocument();
+  });
 });
